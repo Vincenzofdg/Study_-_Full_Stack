aa84d0ecbbfa5ca7a2cab103b32b3886c41f7eca
Vincenzo Fedzuirek Di Giacomo <vincenzofdg@hotmail.com>
2021-10-28 16:15:35 -0300

    Exercicio 01

---
+++ Bloco 07 - Context API e React Hooks/01 - Context API do React (27.10.21)/[EXERCICIO_01]/src/Cars.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import CarContext from './context/CarContext';
+
+import carBlue from './images/carBlue.jpeg';
+import carRed from './images/carRed.jpeg';
+import carYellow from './images/carYellow.jpeg';
+
+class Cars extends React.Component {
+  render() {
+    const { red, blue, yellow, moveCar } = this.context;
+    return (
+      <div>
+        <div>
+          <img className={ red ? 'right' : 'left' } src={ carRed } alt="red" />
+          <button type="button" onClick={ () => moveCar('red', !red) }>Move</button>
+        </div>
+        <div>
+          <img className={ blue ? 'right' : 'left' } src={ carBlue } alt="blue" />
+          <button type="button" onClick={ () => moveCar('blue', !blue)}>Move</button>
+        </div>
+        <div>
+          <img className={ yellow ? 'right' : 'left' } src={ carYellow } alt="yellow" />
+          <button type="button" onClick={ () => moveCar('yellow', !yellow) }>Move</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+Cars.contextType = CarContext;
+
+export default Cars;
