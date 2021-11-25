7822862e7c7b30fae5212618dc34f11395126351
Vincenzo <vincenzofdg@hotmail.com>
2021-11-25 14:09:19 -0300

    Instalaçãõ

--- Bloco 01: Docker - Utilizando Containers/01 - Utilizando Containers (25.11.21)/Install.sh
+++ Bloco 01: Docker - Utilizando Containers/01 - Utilizando Containers (25.11.21)/Install.sh
@@ -26,10 +26,10 @@ for program in ${apt[@]}; do
 done
 
 # Adicionando a chave do Repositório:
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Adicionando o repositório:
-#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Instalando Docker Engine:
 sudo apt update
