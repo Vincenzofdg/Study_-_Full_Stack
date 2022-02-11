244e3f22172d8cbeb0968203c94cd53671944d24
Vincenzofdg <vfdgiacomo@gmail.com>
2022-02-11 14:52:38 -0300

    Aula ao Vivo

--- Bloco 03 - Funções SQL, JOINs e Normalização/04 - Projeto: One for All (04.01.2022)/Desafio11.sql
+++ Bloco 03 - Funções SQL, JOINs e Normalização/04 - Projeto: One for All (04.01.2022)/Desafio11.sql
@@ -10,3 +10,27 @@ SELECT
 FROM SpotifyClone.cancoes
 WHERE nome LIKE 'Streets|Her Own|Inner Fire|Silly|Circus'
 ORDER BY nome;
+
+
+-- FONT https://stackoverflow.com/questions/4271186/how-can-i-use-mysql-replace-to-replace-strings-in-multiple-records
+
+SELECT
+	c.cancao AS nome_musica,
+	REPLACE(
+		REPLACE(
+			REPLACE(
+				REPLACE(
+					REPLACE(c.cancao, 'Streets', 'Code Review'),
+				'Her Own', 'Trybe'),
+			'Inner Fire', 'Project'),
+		'Silly', 'Nice'),
+	'Circus', 'Pull Request') AS novo_nome
+FROM SpotifyClone.cancoes AS c
+WHERE c.cancao LIKE '%Streets'
+OR c.cancao LIKE '%Her Own'
+OR c.cancao LIKE '%Inner Fire'
+OR c.cancao LIKE '%Silly'
+OR c.cancao LIKE '%Circus'
+ORDER BY nome_musica;
+
+
