0215edee92cb4dd11c9ad20caa8a162fe7423f7d
Vincenzo F. Di Giacomo <79121466+Vincenzofdg@users.noreply.github.com>
2022-02-13 12:29:16 -0300

    Exercicio

---
+++ Bloco 04 - Introdução ao desenvolvimento Web com Node.js/02 - Fluxo Assíncrono (09.02.22)/Exercicios.js
@@ -0,0 +1,2 @@
+// 01. Crie uma função que recebe três parâmetros retorna uma Promise.
+//   - Caso algum dos parâmetros recebidos não seja um número, rejeite a Promise com o motivo "Informe apenas números".
