4cf7ef2155335be81ce1a230aa3129baf89c7cb1
Vincenzofdg <vfdgiacomo@gmail.com>
2022-02-14 12:22:00 -0300

    Exercicio 05

--- Bloco 04 - Introdução ao desenvolvimento Web com Node.js/02 - Fluxo Assíncrono (09.02.22)/Exercicios.js
+++ Bloco 04 - Introdução ao desenvolvimento Web com Node.js/02 - Fluxo Assíncrono (09.02.22)/Exercicios.js
@@ -41,11 +41,95 @@ const exercicio03 = async () => {
   const numbers = Array.from({ length: 3 }).map(randomNumbers);
   
   try {
-    const myResult = await exercicio01(..numbers);
+    const myResult = await exercicio01(...numbers);
     console.log(myResult);
   } catch (err) {
     console.log(err.message)
   }
 };
 
-// 04. 
+// 04. Utilizando o arquivo simpsons.json: 
+//   - Crie uma função que leia todos os dados do arquivo e imprima cada personagem no formato id - Nome.
+//     Por exemplo: 1 - Homer Simpson.
+
+const { readFile } = require('fs').promises;
+
+readFile('./simpsons.json', 'utf-8')
+  .then((conteudo) => JSON.parse(conteudo))   // Convertendo JSON para um array
+  .then((str) => str.map(({ id, name }) => `${id} - ${name}`))
+  .then((array) => array.forEach((nome) => console.log(nome)))
+
+//   - Crie uma função que receba o id de uma personagem como parâmetro e retorne uma Promise que é resolvida com os dados da personagem que possui o id informado. Caso não haja uma personagem com o id informado, rejeite a Promise com o motivo "id não encontrado".
+
+const { readFile } = require('fs').promises;
+
+const acharPeloID = async (gID) => {
+  const json = await readFile('./simpsons.json', 'utf-8');
+  const array = await JSON.parse(json);
+  const findMe = array.find(({ id }) => id == gID);
+
+  findMe ? console.log(findMe.name) : console.log("ID não encontrado");
+}
+
+acharPeloID(4000);
+acharPeloID(3);
+
+//   - Crie uma função que altere o arquivo simpsons.json retirando os personagens com id 10 e 6.
+
+const { readFile, writeFile } = require('fs').promises;
+
+const filtrar01 = async () => {
+  const json = await readFile('./simpsons.json', 'utf-8');
+  const array = await JSON.parse(json);
+  const filter = array.filter(({ id }) => id !== '6' && id !== '10');
+  await writeFile('./newFile.json', JSON.stringify(filter));
+}
+
+filtrar01()
+
+//   - Crie uma função que leia o arquivo simpsons.json e crie um novo arquivo, chamado simpsonFamily.json, contendo as personagens com id de 1 a 4.
+
+const { readFile, writeFile } = require('fs').promises;
+
+const filtrar02 = async () => {
+  const json = await readFile('./simpsons.json', 'utf-8');
+  const array = await JSON.parse(json);
+  const filter = array.filter(({ id }) => ['1', '2', '3', '4'].includes(id));
+  await writeFile('./simpsonFamily.json', JSON.stringify(filter));
+}
+
+filtrar02()
+
+//   - Crie uma função que adicione ao arquivo simpsonFamily.json o personagem Nelson Muntz.
+
+const { readFile, writeFile } = require('fs').promises;
+
+const filtrar03 = async () => {
+  const json = await readFile('./simpsonFamily.json', 'utf-8');
+  const array = await JSON.parse(json);
+  array.push({ id: `${array.length + 1}`, name: 'Nelson Muntz' })
+
+  await writeFile('./simpsonFamily.json', JSON.stringify(array));
+}
+
+filtrar03()
+
+//   - Crie uma função que substitua o personagem Nelson Muntz pela personagem Maggie Simpson no arquivo simpsonFamily.json.
+
+const { readFile, writeFile } = require('fs').promises;
+
+const filtrar04 = async () => {
+  const json = await readFile('./simpsonFamily.json', 'utf-8');
+  const array = await JSON.parse(json);
+  const filter = array.filter(({ name }) => name === 'Nelson Muntz');
+  filter[0].name = 'Maggie Simpson'
+  
+  await writeFile('./simpsonFamily.json', JSON.stringify(array));
+}
+
+filtrar04()
+
+// 05. Crie uma função que lê e escreve vários arquivos ao mesmo tempo.
+//   - Utilize o Promise.all para manipular vários arquivos ao mesmo tempo;
+//   - Dado o seguinte array de strings: ['Finalmente', 'estou', 'usando', 'Promise.all', '!!!'] Faça com que sua função crie um arquivo contendo cada string, sendo o nome de cada arquivo igual a file<index + 1>.txt. Por exemplo, para a string "Finalmente", o nome do arquivo é file1.txt.
+//   - Programe sua função para que ela faça a leitura de todos os arquivos criados no item anterior, armazene essa informação e escreva em um arquivo chamado fileAll.txt.
