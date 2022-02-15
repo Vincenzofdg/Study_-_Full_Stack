b88f20f8fb1d13a8cc67ef13b0a3c163b39393a8
Vincenzo <vincenzofdg@hotmail.com>
2022-02-15 19:26:16 -0300

    App Express

---
+++ M.03-04 - Introdução ao desenvolvimento Web com Node.js/04 - Express: HTTP com Node.js (11.02.22)/express-application/README.md
@@ -0,0 +1,3 @@
+1. npm init -y
+2. npm i express
+3. touch index.js
---
+++ M.03-04 - Introdução ao desenvolvimento Web com Node.js/04 - Express: HTTP com Node.js (11.02.22)/express-application/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+// 01. Criando aplicação Express:
+const app = express();
+
+// 02. Quando uma requisição GET for recebida em '/hello' myRequest será chamada
+app.get('/hello', myRequest);
+
+// 03. Pedir ao Express para criar um servidor HTTP e escute a porta 3001
+app.listen(3001, () => console.log('Aplicação ouvindo na porta 3001')); 
+
+// 04. Ao tratar a requisição GET enviar status HTTP 200 (ok) e a mensagem
+function myRequest(req, res) {
+  res.status(200).send('Hello World!'); // 4
+}
+
+// Obs: arrow function não funcionou
