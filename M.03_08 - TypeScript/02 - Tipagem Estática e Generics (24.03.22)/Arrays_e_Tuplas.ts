efa722778e5b8ae7832a116be4c69f12c47fae13
Vincenzofdg <vfdgiacomo@gmail.com>
2022-03-27 17:18:44 -0300

    Arrays e Type

---
+++ M.03_08 - TypeScript/02 - Tipagem Estática e Generics (24.03.22)/Arrays_e_Tuplas.ts
@@ -0,0 +1,11 @@
+// Array
+// Sintaxe: let arrayName: type[] = [...]
+
+let names: string[] = ["Mary Joe", "Alan Joe"];
+
+// Tuplas:
+// Sintaxe: let arrayName: [type, type, ...] = [...]
+
+let fullName: [string, string] = ["Jane", "Doe"];
+let person: [string, number] = ["Jane Doe", 35];
+let car: [string, string, number] = ["Ford", "F400", 10];
