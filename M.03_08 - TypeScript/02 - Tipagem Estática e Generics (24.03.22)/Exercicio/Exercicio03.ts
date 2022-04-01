957c3b216bdfaaae80609837343505053bc1adfc
Vincenzofdg <vfdgiacomo@gmail.com>
2022-03-31 21:12:15 -0300

    Exercicio 03

--- M.03_08 - TypeScript/02 - Tipagem Estática e Generics (24.03.22)/Exercicio/Exercicio03.ts
+++ M.03_08 - TypeScript/02 - Tipagem Estática e Generics (24.03.22)/Exercicio/Exercicio03.ts
@@ -1,19 +1,22 @@
 type Slice = 4 | 6 | 8;
 
+interface Pizza {
+  _flavor: string,
+  _slice: Slice
+}
+
 class Pizza {
-  _flavor: string;
-  _slice: Slice;
   constructor(flavor: string, slice: Slice) {
     this._flavor = flavor;
     this._slice = slice;
   };
 
-  created(): void { 
+  log(): void { 
     const {_slice: slice, _flavor: flavor} = this;
     console.log(`Pizza sabor ${flavor} de ${slice} fatias`)
   };
 };
 
-const pizza01 = new Pizza('Calabresa', 8).created();
-const pizza02 = new Pizza('Marguerita', 6).created();
-const pizza03 = new Pizza('Nutela', 4).created();
+const pizza01 = new Pizza('Calabresa', 8).log();
+const pizza02 = new Pizza('Marguerita', 6).log();
+const pizza03 = new Pizza('Nutela', 4).log();
