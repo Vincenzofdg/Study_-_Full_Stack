535be8aa7660303d5f920e2d82bf2a04b9cfbd5d
Vincenzofdg <vfdgiacomo@gmail.com>
2022-04-08 15:04:10 -0300

    Exemplo 02 Completo

--- M.03_09 - Programação Orientada a Objetos/01 - Introdução (04.04.21)/exemplo02.ts
+++ M.03_09 - Programação Orientada a Objetos/01 - Introdução (04.04.21)/exemplo02.ts
@@ -26,12 +26,11 @@ class Email {
 }
 
 class MailList {
-  private mailList: Email[];
+  private mailList;
 
-  constructor(list: []) {
+  constructor(list: Email[]) {
     this.mailList = list;
   }
-  // constructor(private mailList: Email[] = []) { }
 
   get all(): Email[] { return this.mailList; }
 
@@ -53,3 +52,70 @@ class MailList {
     this.mailList = this.mailList.filter((mail) => mail !== mailToRemove);
   }
 }
+
+// Para testar
+const email01 = new Email(
+  'me@personalmail.com',
+  'friend@personalmail.com',
+  'Car wash',
+  'There\'s a new car wash in town. Did you know?'
+);
+const email02 = new Email(
+  'friend@personalmail.com',
+  'me@personalmail.com',
+  'Reply - Car wash',
+  'Great! My car is really dirty. How much do they charge?'
+);
+const email03 = new Email(
+  'me@personalmail.com',
+  'friend@personalmail.com',
+  'Reply - Reply - Car wash',
+  '$5.00, and you get a free polishing cloth'
+);
+const email04 = new Email(
+  'me@companymail.com',
+  'ceo@companymail.com',
+  'Day off monday',
+  `Next Tuesday is a holiday.
+  Are we going to work on Monday?`
+);
+const email05 = new Email(
+  'ceo@companymail.com',
+  'me@companymail.com',
+  'Reply - Day off monday',
+  'The entire company will have a day off on monday =D'
+);
+
+const personalMailList = new MailList([email01, email02, email03]);
+const professionalMailList = new MailList([email04, email05]);
+
+console.log('------ personalMailList.getByEmailFrom(\'me@personalmail.com\'):');
+personalMailList.getByEmailFrom('me@personalmail.com').forEach(
+  (mail) => console.log(mail.content)
+);
+
+const email6 = new Email(
+  'me@personalmail.com',
+  'friend2@personalmail.com',
+  'Day off monday',
+  `Oh yes!
+   My boss says we\'ll have Monday off.
+   Want to go to the movies?`
+);
+
+personalMailList.addEmail(email6);
+personalMailList.addEmail(email05); // Adicionado erroneamente
+
+console.log('------ personalMailList.getBySubject(\'Day off\'):');
+personalMailList.getBySubject('Day off').forEach(
+  (mail) => console.log(mail.content)
+);
+personalMailList.removeEmail(email05);
+
+console.log('------ professionalMailList.all:');
+professionalMailList.all.forEach((mail) => console.log(mail.content));
+
+console.log('------ personalMailList.all:');
+personalMailList.all.forEach(
+  (mail) => console.log(mail.content)
+);
