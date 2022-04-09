6da0b46081e15c4bde04572dfe24f84d8cda6b69
Vincenzofdg <vfdgiacomo@gmail.com>
2022-04-09 13:49:51 -0300

    Interface Sintaxe

--- M.03_09 - Programação Orientada a Objetos/02 - Herança e Interfaces (05.04.21)/Herança_Sintax.ts
+++ M.03_09 - Programação Orientada a Objetos/02 - Herança e Interfaces (05.04.21)/Herança_Sintax.ts
@@ -8,7 +8,6 @@ class Animal {
   constructor(public name: string, private birthDate: Date) { }
 
   get age() {
-    const birthday = Date.parse(this.birthDate)
     const timeDiff = Math.abs(Date.now() - new Date(this.birthDate).getTime()); // milissegundos
 
     return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25); // Convertendo de volta para anos inteiros, considerando anos bissextos.
@@ -16,14 +15,20 @@ class Animal {
 }
 
 class Mammal extends Animal {
-  walk() {
-    console.log(`${this.name} está andando!`);
-  }
+  walk() { console.log(`${this.name} está andando!`); }
+}
+
+class Bird extends Animal {
+  fly() { console.log(`${this.name} está voando!`); }
 }
 
 const tiger = new Mammal('Tigre', new Date(Date.parse('May 03, 2018')));
+const parrot = new Bird('Papagaio', new Date(Date.parse('Jun 07, 2017')));
 
 const main = (animal: Animal) => console.log(animal.age);
 
 main(tiger); // 3
 tiger.walk(); // Tigre está andando!
+
+main(parrot); // 3
+parrot.fly(); // Papagaio está voando!
